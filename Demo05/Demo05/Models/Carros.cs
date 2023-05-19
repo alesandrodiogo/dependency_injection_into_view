553481cc0b2f3ad83b55bda0033acbe615f18715
Alesandro <alesandromendesdiogo24@gmail.com>
2023-05-19 11:38:06 +0100

    first commit

---
+++ Demo05/Demo05/Models/Carros.cs
@@ -0,0 +1,18 @@
+﻿namespace Demo05.Models
+{
+    public class Carros
+    {
+        public string Nome { get; set; }
+        public string Marca { get; set; }
+
+        public List<Carros> ListarCarros()
+        {
+            var resultado = new  List<Carros>();
+            resultado.Add(new Carros {  Nome = "Kia", Marca = "Cerato" });
+            resultado.Add(new Carros { Nome = "Hiunday", Marca = "Elantra" });
+            resultado.Add(new Carros { Nome = "Suzuki", Marca = "Celero" });
+
+            return resultado;
+        }
+    }
+}
